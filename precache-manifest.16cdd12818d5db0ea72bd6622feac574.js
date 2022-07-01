f8ce4d637662b64d711aec6ae8b8ccf75c0ecea8
unknown <vinod.patil377@gmail.com>
2022-07-01 15:57:28 +0530

    Updates

---
+++ precache-manifest.16cdd12818d5db0ea72bd6622feac574.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "846abac4909096e243df",
+    "url": "/realkey/static/css/main.88ad62eb.chunk.css"
+  },
+  {
+    "revision": "846abac4909096e243df",
+    "url": "/realkey/static/js/main.846abac4.chunk.js"
+  },
+  {
+    "revision": "89fa98e6b34b7efc1fab",
+    "url": "/realkey/static/css/1.5faed659.chunk.css"
+  },
+  {
+    "revision": "89fa98e6b34b7efc1fab",
+    "url": "/realkey/static/js/1.89fa98e6.chunk.js"
+  },
+  {
+    "revision": "8d880c7fa2b11d8b44e1",
+    "url": "/realkey/static/js/runtime~main.8d880c7f.js"
+  },
+  {
+    "revision": "922795b9a8d2c58d87203a41dd7e4006",
+    "url": "/realkey/static/media/thumbnail.922795b9.png"
+  },
+  {
+    "revision": "b8c3ce8d243f4c62d6dec700dfa69d5c",
+    "url": "/realkey/static/media/appkey.b8c3ce8d.png"
+  },
+  {
+    "revision": "b5112f92db421f2c1b18b4a253154865",
+    "url": "/realkey/static/media/android.b5112f92.png"
+  },
+  {
+    "revision": "d2ba4ef83e0e83138d3ac46c0288dd04",
+    "url": "/realkey/static/media/apple.d2ba4ef8.png"
+  },
+  {
+    "revision": "2334e77c8791f5aac5a7cbdb6b073d32",
+    "url": "/realkey/static/media/ajentfinder.2334e77c.png"
+  },
+  {
+    "revision": "b141f7aad37183cc537ff295614a150b",
+    "url": "/realkey/static/media/homefinder.b141f7aa.png"
+  },
+  {
+    "revision": "30cfb030f3ccac2544f0b49257e6068a",
+    "url": "/realkey/static/media/morgage.30cfb030.png"
+  },
+  {
+    "revision": "bfbfe5a948d52b6f6894b279554efb2f",
+    "url": "/realkey/index.html"
+  }
+];
